6db1bea1765f707aaec18592d4ce107e6a928520
Jordan <121682392+jordan-sissilian@users.noreply.github.com>
2024-05-26 15:57:42 +0200

    .

--- Golf_SwingSense/SwingDetect/ContentView.swift
+++ Golf_SwingSense/SwingDetect/ContentView.swift
@@ -1,14 +1,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    @StateObject private var motionManager = SensorsData()
+    //@StateObject private var motionManager = SensorsData()
 
     var body: some View {
         VStack() {
             AcceuilView()
         }
         .onAppear() {
-            motionManager.startUpdates()
+            //motionManager.startUpdates()
         }
     }
 }
+
