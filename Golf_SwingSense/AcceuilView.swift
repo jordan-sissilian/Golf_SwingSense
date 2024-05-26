d31a5b0691640a606204784cb51d3b46c68973a9
Jordan <121682392+jordan-sissilian@users.noreply.github.com>
2024-05-26 12:42:47 +0200

    .

---
+++ Golf_SwingSense/AcceuilView.swift
@@ -0,0 +1,52 @@
+//
+//  AcceuilView.swift
+//  SwingDetect
+//
+//  Created by Jordan on 26/05/2024.
+//
+
+import SwiftUI
+
+struct AcceuilView: View {
+    var body: some View {
+        VStack () {
+            HStack () {
+                VStack () {
+                    Text("lorem ipsum")
+                }
+            }
+            HStack () {
+                VStack () {
+                    Text("lorem ipsum")
+                }
+                VStack () {
+                    Text("lorem ipsum")
+                }
+            }
+        }
+        VStack () {
+            VStack () {
+                HStack () {
+                    Text("lorem ipsum")
+                }
+                HStack () {
+                    VStack () {
+                        Text("lorem ipsum")
+                        Text("lorem ipsum")
+                    }
+                    VStack () {
+                        Text("lorem ipsum")
+                        Text("lorem ipsum")
+                    }
+                }
+                HStack () {
+                    Button("lorem") { }
+                    Button("lorem") { }
+                }
+            }
+            HStack () {
+                Button("lorem") { }
+            }
+        }
+    }
+}
--- Golf_SwingSense/SwingDetect/SensorsSwing/SensorsData.swift
+++ Golf_SwingSense/SwingDetect/SensorsSwing/SensorsData.swift
@@ -6,12 +6,16 @@ class SensorsData: ObservableObject {
 
     @Published var rotationRate: CMRotationRate = CMRotationRate(x: 0, y: 0, z: 0)
     @Published var acceleration: CMAcceleration = CMAcceleration(x: 0, y: 0, z: 0)
-    @Published var magneticField: CMMagneticField = CMMagneticField(x: 0, y: 0, z: 0)
 
-    var posData: (x: Double, y: Double, z: Double) = (0, 0, 0)
     var gyroData: (pitch: Double, roll: Double, yaw: Double) = (0, 0, 0)
-    var accelerometerData: (x: Double, y: Double, z: Double, speed: (speedX: Double, speedY: Double, speedZ: Double)) = (0, 0, 0, (0, 0, 0))
-    var magnetometerData: (x: Double, y: Double, z: Double) = (0, 0, 0)
+
+    private var lastUpdate: Date? = nil
+
+    // Kalman filter state variables
+    private var x: [Double] = [0, 0, 0] // [pitch, roll, yaw]
+    private var P: [[Double]] = [[1, 0, 0], [0, 1, 0], [0, 0, 1]] // Covariance
+    private let Q: [[Double]] = [[0.001, 0, 0], [0, 0.001, 0], [0, 0, 0.001]] // Process noise covariance
+    private let R: [[Double]] = [[0.01, 0, 0], [0, 0.01, 0], [0, 0, 0.01]] // Measurement noise covariance
 
     init() {
         startUpdates()
@@ -19,102 +23,116 @@ class SensorsData: ObservableObject {
 
     func startUpdates() {
         if motionManager.isGyroAvailable {
-            motionManager.gyroUpdateInterval = 0.1
+            motionManager.gyroUpdateInterval = 0.01
             motionManager.startGyroUpdates(to: .main) { [weak self] (data, error) in
-                guard let self = self, let data = data else { return }
+                guard let self = self, let data = data else {
+                    if let error = error {
+                        print("Gyro error: \(error.localizedDescription)")
+                    }
+                    return
+                }
                 DispatchQueue.main.async {
-                    // Intégration des vitesses angulaires pour obtenir la rotation
-                    let pitch = self.gyroData.pitch + data.rotationRate.x
-                    let roll = self.gyroData.roll + data.rotationRate.y
-                    let yaw = self.gyroData.yaw + data.rotationRate.z
-
-                    // Stockage des valeurs dans gyroData
                     self.rotationRate = data.rotationRate
-                    self.gyroData = (pitch: pitch, roll: roll, yaw: yaw)
+                    //self.updateKalmanFilter(gyro: data.rotationRate)
                 }
             }
+        } else {
+            print("Gyroscope not available.")
         }
 
         if motionManager.isAccelerometerAvailable {
-                   motionManager.accelerometerUpdateInterval = 0.01
-                   motionManager.startAccelerometerUpdates(to: .main) { [weak self] (data, error) in
-                       guard let self = self, let data = data else { return }
-                       DispatchQueue.main.async {
-                           self.acceleration = data.acceleration
-
-                           self.accelerometerData = (x: data.acceleration.x, y: data.acceleration.y, z: data.acceleration.z, (0, 0, 0))
-
-                           let accelerationThreshold = 1.0
-                           if abs(data.acceleration.x) > accelerationThreshold ||
-                              abs(data.acceleration.y) > accelerationThreshold ||
-                              abs(data.acceleration.z) > accelerationThreshold {
-
-                               // Calculez la magnitude de l'accélération
-                               let magnitude = sqrt(pow(data.acceleration.x, 2) + pow(data.acceleration.y, 2) + pow(data.acceleration.z, 2))
-
-                               // Vérifiez si la magnitude est non nulle pour éviter une division par zéro
-                               guard magnitude != 0 else {
-                                   return
-                               }
-
-                               // Calculez les composantes de vitesse dans les directions X, Y et Z en multipliant chaque composante d'accélération par la magnitude
-                               let speedX = data.acceleration.x * magnitude
-                               let speedY = data.acceleration.y * magnitude
-                               let speedZ = data.acceleration.z * magnitude
-
-                               // Mettez à jour les valeurs de speedX, speedY et speedZ dans accelerometerData.speed
-                               self.accelerometerData.speed = (speedX: speedX, speedY: speedY, speedZ: speedZ)
-
-                           } else {
-                               self.accelerometerData.speed = (0, 0, 0)
-                           }
-                           self.estimatePosition()
-                           self.printDat()
-                       }
-                   }
-               }
-
-        if motionManager.isMagnetometerAvailable {
-            motionManager.magnetometerUpdateInterval = 0.05
-            motionManager.startMagnetometerUpdates(to: .main) { [weak self] (data, error) in
-                guard let self = self, let data = data else { return }
+            motionManager.accelerometerUpdateInterval = 0.01
+            motionManager.startAccelerometerUpdates(to: .main) { [weak self] (data, error) in
+                guard let self = self, let data = data else {
+                    if let error = error {
+                        print("Accelerometer error: \(error.localizedDescription)")
+                    }
+                    return
+                }
                 DispatchQueue.main.async {
-                    self.magneticField = data.magneticField
-                    self.magnetometerData = (self.magneticField.x, self.magneticField.y, self.magneticField.z)
+                    self.acceleration = data.acceleration
+                    self.updateKalmanFilter(acc: data.acceleration)
                 }
             }
+        } else {
+            print("Accelerometer not available.")
         }
     }
-    
 
-    func estimatePosition() {
-        // Estimer l'orientation à partir des données du gyroscope
-        let pitch = gyroData.pitch
-        let roll = gyroData.roll
-        let yaw = gyroData.yaw
+    func updateKalmanFilter(acc: CMAcceleration? = nil, gyro: CMRotationRate? = nil) {
+        print("\n\n\n\n")
+        // Calculate the time elapsed since the last update
+        let now = Date()
+        var deltaTime: TimeInterval = 0.01
+        if let lastUpdate = self.lastUpdate {
+            deltaTime = now.timeIntervalSince(lastUpdate)
+        }
+        self.lastUpdate = now
 
-        // Intégrer la vitesse pour obtenir la position
-        let positionX = accelerometerData.x
-        let positionY = accelerometerData.y
-        let positionZ = accelerometerData.z
-        
-        // Transformer les changements de position relatifs en position absolue dans l'espace en fonction de l'orientation
-        let absolutePositionX = positionX * cos(roll) * cos(pitch) + positionY * (cos(roll) * sin(pitch) * sin(yaw) - sin(roll) * cos(yaw)) + positionZ * (cos(roll) * sin(pitch) * cos(yaw) + sin(roll) * sin(yaw))
-        let absolutePositionY = positionX * sin(roll) * cos(pitch) + positionY * (sin(roll) * sin(pitch) * sin(yaw) + cos(roll) * cos(yaw)) + positionZ * (sin(roll) * sin(pitch) * cos(yaw) - cos(roll) * sin(yaw))
-        let absolutePositionZ = positionX * -sin(pitch) + positionY * cos(pitch) * sin(yaw) + positionZ * cos(pitch) * cos(yaw)
-        
-        self.posData = (absolutePositionX, absolutePositionY, absolutePositionZ)
-    }
-    
-    func printDat() {
-        print("\(String(format: "%.1f", posData.x)) \(String(format: "%.1f", posData.y)) \(String(format: "%.1f", posData.z))")
+        // Prediction step using gyro data
+        if let gyro = gyro {
+            x[0] += gyro.x * deltaTime
+            x[1] += gyro.y * deltaTime
+            x[2] += gyro.z * deltaTime
+        }
+        var P_pred = P
+        for i in 0..<3 {
+            for j in 0..<3 {
+                P_pred[i][j] += Q[i][j]
+            }
+        }
+
+        // Update step using accelerometer data
+        if let acc = acc {
+            let z = [atan2(acc.y, acc.z), atan2(-acc.x, acc.z), atan2(acc.y, acc.x)]
+            let y = [z[0] - x[0], z[1] - x[1], z[2] - x[2]]
+            var S = [[Double]](repeating: [Double](repeating: 0, count: 3), count: 3)
+            for i in 0..<3 {
+                for j in 0..<3 {
+                    S[i][j] = P_pred[i][j] + R[i][j]
+                }
+            }
+            var K = [[Double]](repeating: [Double](repeating: 0, count: 3), count: 3)
+            for i in 0..<3 {
+                for j in 0..<3 {
+                    K[i][j] = P_pred[i][j] / S[i][j]
+                }
+            }
+            for i in 0..<3 {
+                x[i] += K[i][i] * y[i]
+            }
+            for i in 0..<3 {
+                for j in 0..<3 {
+                    P[i][j] = (1 - K[i][i]) * P_pred[i][j]
+                }
+            }
+
+            print("Accelerometer Data: \(acc)")
+            print("Gyroscope Data: \(String(describing: gyro))")
+
+            print("Predicted State (before update): \(x)")
+            print("Predicted Covariance (before update): \(P_pred)")
+
+            // Update state using Kalman filter
+            print("Measured State: \(z)")
+            print("Innovation (Residual): \(y)")
+            print("Innovation Covariance: \(S)")
+            print("Kalman Gain: \(K)")
+
+            print("Updated State (after correction): \(x)")
+            print("Updated Covariance (after correction): \(P)")
+        }
+
+        self.gyroData = (pitch: x[0], roll: x[1], yaw: x[2])
+
+        // Print updated orientation data
+        print("Pitch: \(x[0])")
+        print("Roll: \(x[1])")
+        print("Yaw: \(x[2])")
     }
 
     func stopUpdates() {
         motionManager.stopAccelerometerUpdates()
         motionManager.stopGyroUpdates()
-        motionManager.stopMagnetometerUpdates()
     }
-    
 }
-
